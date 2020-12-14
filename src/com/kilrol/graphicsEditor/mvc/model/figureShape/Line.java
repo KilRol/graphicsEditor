9d647a7f6a26b5033c4c68804790bd1a37d207a8
KilRol <sniper.lech@gmail.com>
2020-12-14 16:00:13 +0300

    #

--- src/com/kilrol/graphicsEditor/mvc/model/figureShape/Line.java
+++ src/com/kilrol/graphicsEditor/mvc/model/figureShape/Line.java
@@ -6,11 +6,6 @@ import java.awt.geom.Point2D;
 public class Line extends Line2D.Double implements FigureShape {
 
     @Override
-    public void setFrameFromDiagonal(Point2D point2D, Point2D point2D1) {
-        setLine(point2D, point2D1);
-    }
-
-    @Override
     public double getMinX() {
         return getX1();
     }
@@ -27,11 +22,16 @@ public class Line extends Line2D.Double implements FigureShape {
 
     @Override
     public double getMaxY() {
-        return getX2();
+        return getY2();
     }
 
     @Override
     public void setFrameFromDiagonal(double xMin, double yMin, double xMax, double yMax) {
         setLine(xMin, yMin, xMax, yMax);
+    }
+
+    @Override
+    public void setFrameFromDiagonal(Point2D point2D, Point2D point2D1) {
+        setLine(point2D, point2D1);
     }
 }
